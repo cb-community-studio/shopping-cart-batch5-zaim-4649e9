6e74bc271d8875d5e3d5bebde258f7056a205416
Code Bridge Machine <ahmed@codebridge.my>
2023-05-06 14:51:42 +0800

    CodeBridge commit

---
+++ nodejs-backend/src/services/index.js
@@ -0,0 +1,12 @@
+const users = require("./users/users.service.js");
+const product = require("./product/product.service.js");
+const cart = require("./cart/cart.service.js");
+// ~cb-add-require-service-name~
+
+// eslint-disable-next-line no-unused-vars
+module.exports = function (app) {
+  app.configure(users);
+  app.configure(product);
+  app.configure(cart);
+  // ~cb-add-configure-service-name~
+};
